1c9bb16220a94b7413ba686a9d9be32d13ef7629
Francisco Soares <neppersac@yahoo.com.br>
2019-08-30 16:42:22 -0300

    Update inserir_dados.sql
    Organização dos arquivo

--- inserir_dados.sql
+++ inserir_dados.sql
@@ -1,2 +1,19 @@
+/** Inserir dados na tablea de contatos */
 INSERT INTO contatos (fone, email) VALUES ( "2222222", "email@gmail.com");
 INSERT INTO contatos (fone, email) VALUES ( "3333333", "paciente@gmail.com");
+
+/** Inserir dados na tablea de convenios */
+INSERT INTO convenios (numero, nome) VALUES ( "2", "AMIL");
+INSERT INTO convenios (numero, nome) VALUES ( "1", "SUS");
+
+/** Inserir dados na tablea de medicos */
+
+
+/** Inserir dados na tablea de pacientes */
+
+/** Inserir dados na tablea de exames */
+
+/** Inserir dados na tablea de consultas */
+
+
+
