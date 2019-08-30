781c90a2cb01d2c2e86136f649295964c5a1c14f
Francisco Soares <neppersac@yahoo.com.br>
2019-08-30 16:36:35 -0300

    Update inserir_dados.sql
    Inclusão de uma linha

--- inserir_dados.sql
+++ inserir_dados.sql
@@ -1,2 +1,2 @@
-INSERT INTO contatos (fone, email)
-VALUES ( "2222222", "email@gmai");
+INSERT INTO contatos (fone, email) VALUES ( "2222222", "email@gmail.com");
+INSERT INTO contatos (fone, email) VALUES ( "3333333", "paciente@gmail.com");
