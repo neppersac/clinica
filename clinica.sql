b6efba5896e15afa3191c7b4d63b0f78b923515d
M4G0G4ND0LF <54723478+M4G0G4ND0LF@users.noreply.github.com>
2019-08-30 16:32:21 -0300

    Update clinica.sql
    criei a tabela de Convênio

--- clinica.sql
+++ clinica.sql
@@ -1,17 +1,13 @@
 CREATE DATABASE clinica;
 use clinica;
 create table contatos(
- 	id int not null auto_increment primary key,
+  id int not null auto_increment primary key,
 	fone varchar(15),
 	email varchar(100)
 );
-create table enderecos(
-	id int not null auto_incremet primary key,
-	logradouro varchar(100),
-	bairro varchar(50),
-	cidade varchar(50),
-	uf varchar(2),
-	cep varchar(10)
+create table convenios(
+	id int not null auto_increment primary key,
+	numero varchar(15),
+	nome varchar(50)
 );
-	
 
